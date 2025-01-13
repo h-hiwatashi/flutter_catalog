ab2c93b5e69f37c9b8e27765423bc27f5a99abee
h-hiwatashi <hiwatashi.hiromitsu@gmail.com>
2025-01-13 22:47:33 +0900

    CatalogViewにVideoPlayerWidgetを追加し、ビデオ再生機能を実装

--- lib/views/catalog_view.dart
+++ lib/views/catalog_view.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 
+import '../widgets/video_player.dart';
+
 class CatalogView extends StatelessWidget {
   const CatalogView({super.key});
 
@@ -10,9 +12,10 @@ class CatalogView extends StatelessWidget {
       appBar: AppBar(
         title: Text('Catalog View'),
       ),
-      body: Center(
-        child: Text('This is the catalog view'),
-      ),
+      body: VideoPlayerWidget(),
+      // body: Center(
+      //   child: Text('This is the catalog view'),
+      // ),
     );
   }
 }
--- lib/widgets/video_player.dart
+++ lib/widgets/video_player.dart
@@ -1,47 +1,42 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_hooks/flutter_hooks.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 import 'package:video_player/video_player.dart';
 
 /// https://pub.dev/packages/video_player
 /// https://docs.flutter.dev/cookbook/plugins/play-video
 
-class VideoPlayerWidget extends HookConsumerWidget {
-  late VideoPlayerController _controller;
-
+class VideoPlayerWidget extends HookWidget {
   VideoPlayerWidget({super.key});
 
-  useEffect() {
-    _controller = VideoPlayerController.networkUrl(Uri.parse(
-        'https://flutter.github.io/assets-for-api-docs/assets/videos/butterfly.mp4'))
-      ..initialize().then((_) {
-        _controller.play();
-        _controller.setLooping(true);
+  @override
+  Widget build(BuildContext context) {
+    final controller = useMemoized(() => VideoPlayerController.networkUrl(Uri.parse(
+        'https://flutter.github.io/assets-for-api-docs/assets/videos/butterfly.mp4')));
+    final initialized = useState(false);
+    useEffect(() {
+      controller.initialize().then((_) {
+        initialized.value = true;
       });
 
-    return () {
-      _controller.dispose();
-    };
-  }
+      return controller.dispose;
+    }, [controller]);
 
-  @override
-  Widget build(BuildContext context, WidgetRef ref) {
     return Scaffold(
       body: Center(
-        child: _controller.value.isInitialized
+        child: controller.value.isInitialized
             ? AspectRatio(
-                aspectRatio: _controller.value.aspectRatio,
-                child: VideoPlayer(_controller),
+                aspectRatio: controller.value.aspectRatio,
+                child: VideoPlayer(controller),
               )
             : Container(),
       ),
       floatingActionButton: FloatingActionButton(
         onPressed: () {
-          _controller.value.isPlaying
-              ? _controller.pause()
-              : _controller.play();
+          controller.value.isPlaying ? controller.pause() : controller.play();
         },
         child: Icon(
-          _controller.value.isPlaying ? Icons.pause : Icons.play_arrow,
+          controller.value.isPlaying ? Icons.pause : Icons.play_arrow,
         ),
       ),
     );
