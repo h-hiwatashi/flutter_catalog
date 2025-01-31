06580ec69b0484a8e2aa2ae42abb1e50c07052d4
h-hiwatashi <62828304+h-hiwatashi@users.noreply.github.com>
2025-01-31 21:10:40 +0900

    update

--- lib/widgets/button_widgets.dart
+++ lib/widgets/button_widgets.dart
@@ -15,58 +15,125 @@ class ButtonWidgetsView extends ConsumerWidget {
           SliverList(
             delegate: SliverChildBuilderDelegate(
               (BuildContext context, int index) {
-                return Column(
-                  children: [
-                    SizedBox(height: 10),
-                    FloatingActionButton.small(
-                      heroTag: null,
-                      onPressed: () {
-                        // Add your onPressed code here!
-                      },
-                      child: const Icon(Icons.add),
-                    ),
-                    SizedBox(height: 10),
-                    FloatingActionButton(
-                      heroTag: null,
-                      onPressed: () {
-                        // Add your onPressed code here!
-                      },
-                      child: const Icon(Icons.add),
-                    ),
-                    SizedBox(height: 10),
-                    FloatingActionButton.large(
-                      heroTag: null,
-                      onPressed: () {
-                        // Add your onPressed code here!
-                      },
-                      child: const Icon(Icons.add),
-                    ),
-                    SizedBox(height: 10),
-                    FloatingActionButton.extended(
-                      heroTag: null,
-                      onPressed: () {
-                        // Add your onPressed code here!
-                      },
-                      label: const Text('Add'),
-                      icon: const Icon(Icons.add),
-                    ),
-                    SizedBox(height: 10),
-                    TextButton(
-                      onPressed: () {},
-                      child: const Text('TextButton1 Sample'),
-                    ),
-                    SizedBox(height: 10),
-                    OutlinedButton(
-                      onPressed: () {},
-                      child: const Text('OutlinedButton1 Sample'),
-                    ),
-                    SizedBox(height: 10),
-                    ElevatedButton(
-                      onPressed: () {},
-                      child: const Text('ElevatedButton1 Sample'),
-                    ),
-                    SizedBox(height: 10),
-                  ],
+                return Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: Column(
+                    children: [
+                      SizedBox(height: 10),
+                      FloatingActionButton.small(
+                        heroTag: null,
+                        onPressed: () {
+                          // Add your onPressed code here!
+                        },
+                        child: const Icon(Icons.add),
+                      ),
+                      SizedBox(height: 10),
+                      FloatingActionButton(
+                        heroTag: null,
+                        onPressed: () {
+                          // Add your onPressed code here!
+                        },
+                        child: const Icon(Icons.add),
+                      ),
+                      SizedBox(height: 10),
+                      FloatingActionButton.large(
+                        heroTag: null,
+                        onPressed: () {
+                          // Add your onPressed code here!
+                        },
+                        child: const Icon(Icons.add),
+                      ),
+                      SizedBox(height: 10),
+                      FloatingActionButton.extended(
+                        heroTag: null,
+                        onPressed: () {
+                          // Add your onPressed code here!
+                        },
+                        label: const Text('Add'),
+                        icon: const Icon(Icons.add),
+                      ),
+                      SizedBox(height: 10),
+                      TextButton(
+                        onPressed: () {},
+                        child: const Text('TextButton1 Sample'),
+                      ),
+                      SizedBox(height: 10),
+                      OutlinedButton(
+                        onPressed: () {},
+                        child: const Text('OutlinedButton1 Sample'),
+                      ),
+                      SizedBox(height: 10),
+                      ElevatedButton(
+                        onPressed: () {},
+                        child: const Text('ElevatedButton1 Sample'),
+                      ),
+                      SizedBox(height: 10),
+                      IconButton(
+                        icon: const Icon(Icons.volume_up),
+                        padding: EdgeInsets.all(10),
+                        onPressed: () {
+                          const SnackBar(
+                            content: Text('ボタンが押されました！'),
+                          );
+                        },
+                      ),
+                      SizedBox(height: 10),
+                      Text('Text!!!'),
+                      SizedBox(height: 10),
+                      RichText(
+                        text: TextSpan(
+                          style: DefaultTextStyle.of(context).style,
+                          children: [
+                            TextSpan(
+                              text: 'RichText',
+                              style: TextStyle(fontWeight: FontWeight.bold),
+                            ),
+                            TextSpan(text: ' is'),
+                            TextSpan(
+                              text: ' Nice!',
+                              style: TextStyle(fontWeight: FontWeight.bold),
+                            ),
+                          ],
+                        ),
+                      ),
+                      SizedBox(height: 10),
+                      TextField(
+                        decoration: InputDecoration(
+                          border: OutlineInputBorder(),
+                          labelText: 'テキストフィールド',
+                        ),
+                      ),
+                      SizedBox(height: 10),
+                      const Image(
+                        image: NetworkImage(
+                            'https://flutter.github.io/assets-for-api-docs/assets/widgets/owl.jpg'),
+                      ),
+                      SizedBox(height: 10),
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceAround,
+                        children: <Widget>[
+                          const Icon(Icons.search,
+                              size: 30, color: Colors.green),
+                          const Icon(Icons.favorite,
+                              size: 30, color: Colors.red),
+                          const Icon(Icons.settings,
+                              size: 30, color: Colors.grey),
+                          IconButton(
+                            onPressed: () {
+                              print('tap');
+                            },
+                            icon: Icon(
+                              Icons.home,
+                              size: 30,
+                              color: Colors.blue,
+                            ),
+                          )
+                        ],
+                      ),
+                      Divider(),
+                      SizedBox(height: 50),
+                    ],
+                  ),
                 );
               },
               childCount: 1,
