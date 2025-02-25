255c8d8bc5c3559afec47939d31c17965d66c789
h-hiwatashi <hiwatashi.hiromitsu@gmail.com>
2025-02-26 00:57:44 +0900

    DrinkClassを追加し、コーヒーとミルクの生成ロジックを実装

--- lib/domain/sample/logic/logic.dart
+++ lib/domain/sample/logic/logic.dart
@@ -1,5 +1,48 @@
-sealed class SampleSealdClass {
-  const SampleSealdClass._();
-  const factory SampleSealdClass.first() = First;
-  const factory SampleSealdClass.second() = Second;
+import 'package:freezed_annotation/freezed_annotation.dart';
+
+@freezed
+sealed class DrinkClass with _$DrinkClass {
+  factory DrinkClass.fromDto(bool isCoffee) {
+    if (isCoffee) {
+      return const DrinkClass.coffee(
+        name: 'coffee',
+        price: 100,
+        isNeedSugar: true,
+      );
+    } else {
+      return const DrinkClass.milk(
+        name: 'milk',
+        price: 50,
+        isHot: 50,
+      );
+    }
+  }
+
+  const DrinkClass._();
+  const factory DrinkClass.coffee({
+    required String name,
+    required int price,
+    required bool isNeedSugar,
+  }) = _Coffee;
+  const factory DrinkClass.milk({
+    required String name,
+    required int price,
+    required int isHot,
+  }) = _Milk;
 }
+
+// class _Coffee extends DrinkClass {
+//   const _Coffee({
+//     required String name,
+//     required int price,
+//     required bool isNeedSugar,
+//   }) : super._();
+// }
+
+// class _Milk extends DrinkClass {
+//   const _Milk({
+//     required String name,
+//     required int price,
+//     required int isHot,
+//   }) : super._();
+// }
